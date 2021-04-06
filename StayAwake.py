2d3682e9257736406c4c882715841323e1a4239e
Amir Roknifard <roknifard@gmail.com>
2021-04-06 15:25:25 -0600

    Update StayAwake.py
    Updated to use Win32api

--- StayAwake.py
+++ StayAwake.py
@@ -2,11 +2,13 @@ import pyautogui
 import time
 import sys
 from datetime import datetime
-from idle_time import IdleMonitor
+import win32api
 
+def getIdleTime():
+    return (win32api.GetTickCount() - win32api.GetLastInputInfo()) / 1000.0
+   
 pyautogui.FAILSAFE = False
 numMin = None
-monitor = IdleMonitor.get_monitor()
 
 if ((len(sys.argv) < 2) or sys.argv[1].isalpha() or int(sys.argv[1]) < 1):
    numMin = 3
@@ -14,7 +16,7 @@ else:
    numMin = int(sys.argv[1])
 
 while(True):
-  y = monitor.get_idle_time()
+  y = getIdleTime()
   x=0
   while (x<numMin):
       time.sleep(60)
